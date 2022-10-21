acd42708bcb783c834e28048d72f79102879b068
MikeFrancisJr <114704008+MikeFrancisJr@users.noreply.github.com>
2022-10-21 01:46:09 +0000

    Completed Project with Comments

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-### Setup Section ###
-
-# We'll learn about how this line of code works later in the course - for now just know it loads the colored text
+# Create a welcome message for the user
+print("Welcome to The Color of Words!")
+print("====================")
+print("You have 6 attempts to guess the correct word.")
+print("The word is SIX CHARACTERS long (which is why you get SIX tries)!")
+print("So you MUST enter a guess of this length.")
+print("If a letter is in the correct place, it will be green.")
+print("If a letter is in the word, but NOT in the correct place,")
+print("it will be yellow.")
+print("If the letter is NOT in the word, it will be red.")
+print()
+# Create a function that prints out a letter with a colorful background 
 from colorama import Fore, Back, Style
 
-# Function that prints out a letter with a colorful background
 def printColorfulLetter(letter, isLetterInWord, isLetterInCorrectPlace = False):
 
   # If it's not in the word, display it with a red background
@@ -37,49 +45,48 @@ def printGuessAccuracy(guess, actual):
 
       
       # If the letter is in the secret word, is it also AT THE CURRENT INDEX in the secret word?
-      if(letter == secret[position]):
+      if(letter == secret[index]):
 
         # Then print it out with a green background
         printColorfulLetter(letter, True, True)
 
       # If it's not at the current index, we know by this point in the code that it's still used in the secret word somewhere...
       else:
-
         # ...so we'll print it out with a yellow background
-
+        printColorfulLetter(letter, True, False)
     # ...but if the letter is not in the word at all...
     else:
       # ...print it out with a red background
-      
+      printColorfulLetter(letter, False, False)
     # Don't worry about the line of code below, it works. It just handles the transition between colors
     print(Style.RESET_ALL + " ", end="")
 
-# TO-DO: Write a Function that takes in a six-lettered word from the user
+# Write a Function that takes in a six-lettered word from the user
 def userInput():
+  # Create a variable that holds a default value for the users guess
   userGuess = ""
+  # Continue to loop the users guess as long as it's NOT six characters
   while(len(userGuess) != 6):
+    # Ask the user to enter a six letter word
     userGuess = input("Please enter a six letter word: ")
+    # Return the users guess in lower case
   return userGuess.lower()
-
-  
-  
-
-# This marks the end of the function definitions, below this is where the program will actually start!
-
 ### Main Program ###
+# Create a variable that stores the secret word
 secretWord = "flames"
+# Create a variable that counts the number of guess attempts
 countTurns = 0
-
+# Create a variable that holds the default value for the last attempt
 lastTurn = ""
-
+# Create a while loop that loops as long as the guess attemps are less than 6 and NOT the secret word
 while countTurns < 6 and lastTurn != secretWord:
-  lastTurn = getUserInput()
+  lastTurn = userInput()
   printGuessAccuracy(lastTurn, secretWord)
-countTurns += 1
-  if countTurns == 6:
+  countTurns += 1
+# if guesses equals 6, display message to user that they've reached max attempts
+if countTurns == 6:
     print("You've exceeded your max attempts")
-  elif lastTurn == secretWord
+# if user enters the correct word, display 'Good Job' message
+if lastTurn == secretWord:
     print("GOOD JOB!")
-
   
-# TO-DO: Write the logic of the game here!
